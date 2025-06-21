361f91173f75714873d12b741c29aae015ac4aa8
helicopter_45 (aka fotis zaharia) <143888026+fotiszaharia@users.noreply.github.com>
2025-06-21 08:26:06 +0300

    Update boot.py

--- boot.py
+++ boot.py
@@ -1,4 +1,10 @@
-from main import monomaOS
+from main import monoomaOS
+import platform
+import os
+if platform.system() == "Windows":
+    os.system('title monomaOS v0.0.1')
+else:
+    os.system('echo -n -e "\033]0;monomaOS v0.0.1\007"')
 print("welcome to monomaOS")
 input("Press enter to continue...")
-monomaOS()
+monoomaOS()
