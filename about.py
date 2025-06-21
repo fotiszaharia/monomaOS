9eb958bfcc5bdd09997977f4d60df15f11843f82
helicopter_45 (aka fotis zaharia) <143888026+fotiszaharia@users.noreply.github.com>
2025-06-21 08:25:49 +0300

    Update about.py

--- about.py
+++ about.py
@@ -1,10 +1,16 @@
 from main import monoomaOS
-
+import platform
+import os
+if platform.system() == "Windows":
+    os.system('title monomaOS v0.0.1')
+else:
+    os.system('echo -n -e "\033]0;monomaOS v0.0.1\007"')
 
 
 def about_mmonomaOS():
     print("monomaOS is a simple operating system written in Python.")
     print("It is designed to be easy to use and understand.")
     print("Version: 0.0.1")
-    
+    input("Press enter to enter back to monomaOS...")
+
     monoomaOS()
